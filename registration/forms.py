771050b440ed2feb86f38467dbdb8aca6f5325b9
Chris Ogwara <smartchrys@gmail.com>
2016-04-08 18:57:49 +0100

    Edited

--- registration/forms.py
+++ registration/forms.py
@@ -1,16 +1,15 @@
 from django import forms
-from django.contrib.auth.models import User
 from django.forms import ModelForm
 from registration.models import *
 
 
 class RegistrationForm(ModelForm):
-    state_code = forms.CharField(max_length=12, label=u'STATE CODE')
-    first_name = forms.CharField(max_length=200, label=u'FIRST NAME')
-    last_name = forms.CharField(max_length=200, label=u'LAST NAME')
+    state_code = forms.CharField(max_length=12, label=u'State Code')
+    first_name = forms.CharField(max_length=200, label=u'First Name')
+    last_name = forms.CharField(max_length=200, label=u'Last Name')
     email = forms.EmailField(null=True, label=u'Email')
-    password = forms.CharField(label=u'PASSWORD', widget=forms.PasswordInput(render_value=False))
-    re_password = forms.CharField(label=u'CONFIRM PASSWORD', widget=forms.PasswordInput(render_value=False))
+    password = forms.CharField(label=u'Password', widget=forms.PasswordInput(render_value=False))
+    re_password = forms.CharField(label=u'Confirm Password', widget=forms.PasswordInput(render_value=False))
 
     class Meta:
         model = Registration
@@ -33,8 +32,13 @@ class RegistrationForm(ModelForm):
         raise forms.ValidationError("That email is already taken, please select another.")
 
     def clean_password1(self):
-        if self.cleaned_data["password"] != self.cleaned_data["password1"]:
-            raise forms.ValidationError("The passwords did not match or password is too short")
+        if self.cleaned_data["password"] != self.cleaned_data["re_password"]:
+            raise forms.ValidationError("The passwords did not match")
         elif len(self.cleaned_data["password"]) <= 6:
-            raise forms.ValidationError("The password is too short")
+            raise forms.ValidationError("The password is too short, it must be more than 6")
         return self.cleaned_data
+
+
+class LoginForm(forms.Form):
+    state_code = forms.CharField(label=u'State Code')
+    password = forms.CharField(label=u'Password', widget=forms.PasswordInput(render_value=False))
--- registration/views.py
+++ registration/views.py
@@ -1,2 +1,58 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
+from corpers.models import Corper
+from registration.forms import *
+
+
+def corper_registration(request):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect('corpers_profile')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = User.objects.create_user(username=form.cleaned_data['state_code'],
+                                            email=form.cleaned_data['email'],
+                                            password=form.cleaned_date['password'],
+                                            first_name=form.cleaned_data['first_name'],
+                                            last_name=form.cleaned_data['last_name'])
+            user.save()
+            corper = Corper(corper=user, sex=form.cleaned_data['sex'],
+                            state_of_origin=form.cleaned_data['state_of_origin'],
+                            date_of_birth=form.cleaned_data['date_of_birth'],
+                            about=form.cleaned_data['about'],
+                            phone_number=form.cleaned_data['phone_number'],
+                            logo=form.cleaned_data['logo'])
+            corper.save()
+            return HttpResponseRedirect('corpers_profile')
+        else:
+            context = {'form': form}
+            return render_to_response('account/corper_reg.html', context, context_instance=RequestContext(request))
+    """user is not submitting the form , show them a blank account form"""
+    form = RegistrationForm()
+    context = {'form': form}
+    return render_to_response('account/corper_reg.html', context, context_instance=RequestContext(request))
+
+
+def corper_login(request):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect('corpers_profile')
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            state_code = form.cleaned_data['state_code']
+            password = form.cleaned_data['password']
+            corper = authenticate(state_code=state_code, password=password)
+            if corper is not None:
+                login(request, corper)
+                return HttpResponseRedirect('corper_profile')
+            context = {'form': form}
+            return render_to_response('account/login.html', context, context_instance=RequestContext(request))
+        context = {'form': form}
+        return render_to_response('account/login.html', context, context_instance=RequestContext(request))
+    """User is not submitting the form, show the login form """
+    form = LoginForm()
+    context = {'form': form}
+    return render_to_response('account/login.html', context, context_instance=RequestContext(request))
